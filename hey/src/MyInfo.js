abe8960a30b30ca1f5bf4d3cb0545729f90b3647
Bharat <bgbharat787@gmail.com>
2020-05-19 23:37:53 +0530

    react files

---
+++ hey/src/MyInfo.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <h2>My name is bg</h2>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    )
+  }
+
+export default MyInfo
